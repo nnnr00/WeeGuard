8bcfb776ae6e48ab20975903f9c43ddea9a8c041
nnnr <121476007+nnnr00@users.noreply.github.com>
2026-01-13 16:21:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from telegram.ext import (
 
 app = FastAPI()
 BOT_TOKEN = os.environ["BOT_TOKEN"]
-ADMIN_ID = int(os.environ.get("ADMIN_ID", "0"))
+ADMIN_ID = int(os.environ.get("ADMIN_ID", "1480512549"))
 
 WAITING_KEYWORD, WAITING_TYPE, WAITING_CONTENT = range(3)
 
